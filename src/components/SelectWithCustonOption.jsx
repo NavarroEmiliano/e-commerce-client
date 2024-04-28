a47a4c7a802bb4f0eb4288013e2506d01920b178
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-04-28 07:17:51 -0300

    format select

--- src/components/SelectWithCustonOption.jsx
+++ src/components/SelectWithCustonOption.jsx
@@ -1,53 +1,61 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState } from 'react'
 
-const SelectWithCustomOption = ({ label, name, options, handleOnChange, value }) => {
-  const [showCustomInput, setShowCustomInput] = useState(false);
-  const [customOption, setCustomOption] = useState('');
+const SelectWithCustomOption = ({
+  label,
+  name,
+  options,
+  handleOnChange,
+  value
+}) => {
+  const [showCustomInput, setShowCustomInput] = useState(false)
+  const [customOption, setCustomOption] = useState('')
 
-  const handleSelectChange = (e) => {
-    const selectedValue = e.target.value;
+  const handleSelectChange = e => {
+    const selectedValue = e.target.value
     if (selectedValue === 'custom') {
-      setShowCustomInput(true);
+      setShowCustomInput(true)
     } else {
-      setShowCustomInput(false);
-      handleOnChange(e); // Propagar el cambio al componente padre
+      setShowCustomInput(false)
+      handleOnChange(e)
     }
-  };
+  }
 
-  const handleCustomInputChange = (e) => {
-    setCustomOption(e.target.value);
-  };
+  const handleCustomInputChange = e => {
+    setCustomOption(e.target.value)
+  }
 
   const handleAddCustomOption = () => {
-    // Realizar alguna acción adicional si es necesario, como enviar el valor al componente padre
-    handleOnChange({ target: { name, value: customOption } });
+    handleOnChange({ target: { name, value: customOption } })
 
-    setShowCustomInput(false);
-    setCustomOption('');
-  };
-
+    setShowCustomInput(false)
+    setCustomOption('')
+  }
 
   return (
     <div>
       <label>{label}</label>
       <select name={name} onChange={handleSelectChange} value={value}>
-        <option value="">Seleccionar una opción</option>
+        <option value=''>Seleccionar una opción</option>
         {options.map((option, index) => (
           <option key={index} value={option}>
             {option}
           </option>
         ))}
-        <option value="custom">Agregar nueva opción</option>
+        <option value='custom'>Agregar nueva opción</option>
       </select>
       {showCustomInput && (
         <div>
-          <input type="text" value={customOption} onChange={handleCustomInputChange} />
+          <input
+            type='text'
+            value={customOption}
+            onChange={handleCustomInputChange}
+          />
           <button onClick={handleAddCustomOption}>Agregar</button>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default SelectWithCustomOption;
+export default SelectWithCustomOption

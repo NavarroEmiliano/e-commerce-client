43ec1358669215ce6fcb05861270f818b9ad2a1a
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-05-08 17:03:50 -0300

    working

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 const baseUrl = `${import.meta.env.VITE_BASE_URL}/products`
 
-
-console.log('BaseUrl:',import.meta.env.VITE_BASE_URL)
-
 const getAllProducts = async () => {
   try {
     const { data } = await axios.get(baseUrl)

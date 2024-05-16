c4f521b04f48698cb0baafbe19013ce3657fcd96
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-05-16 19:20:06 -0300

    view products

--- src/pages/ViewPurchases.jsx
+++ src/pages/ViewPurchases.jsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import purchaseService from '../services/purchaseService'
 import { useAuthContext } from '../hooks/useAuthContext'
+import moment from 'moment/moment'
 
 const ViewPurchases = () => {
   const { user } = useAuthContext()
@@ -15,7 +16,30 @@ const ViewPurchases = () => {
   })
 
   console.log(userPurchases)
-  return <div>ViewPurchases</div>
+  return (
+    <div>
+      {userPurchases
+        ? userPurchases.map((purchase) => (
+            <div key={purchase.id} className='w-full bg-slate-600'>
+              <div>{moment(purchase.createdAt).format('LL')}</div>
+              <div>
+                {purchase.items.map((item) => (
+                  <div key={item._id} className='h-48'>
+                    <div>
+                      <img
+                        src={item.productId.images[0]}
+                        alt=''
+                        className='w-36'
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))
+        : ''}
+    </div>
+  )
 }
 
 export default ViewPurchases

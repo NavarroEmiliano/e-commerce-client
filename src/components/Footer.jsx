525d5fca1bb249d988bd38360f7ebac9f2494b2d
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-05-28 10:04:33 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ const Footer = () => {
   return (
     <footer className='bg-pink-600'>
       <div className='contain mx-auto p-4'>
-        <p className='text-center'>Developed by Emiliano Navarro</p>
+        <p className='text-center text-pink-900'>
+          Developed by Emiliano Navarro
+        </p>
       </div>
     </footer>
   )

90b3c00b7901fc44d2fda6df04c2f1643d3ca27c
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-05-29 06:03:55 -0300

    skeleton categories

--- src/pages/CategoryProduct.jsx
+++ src/pages/CategoryProduct.jsx
@@ -3,39 +3,32 @@ import { useNavigate, useParams } from 'react-router-dom'
 import ProductCard from '../components/ProductCard'
 import { useQuery } from '@tanstack/react-query'
 import productsService from '../services/productsService'
+import Skeleton from 'react-loading-skeleton'
 
 const CategoryProduct = () => {
   const { categoryName } = useParams()
 
-  /*   const [sortByPrice, setSortByPrice] = useState('')
-   */
-
-  const { isPending, error, data } = useQuery({
+  const { isPending, data } = useQuery({
     queryKey: [categoryName],
     queryFn: () => productsService.getProductsByCategory(categoryName),
     enabled: !!categoryName,
   })
 
-  /*   const filteredProducts = [...productsByCategory]
-    .sort((a, b) => {
-      if (sortByPrice === 'ASC') {
-        return a.price - b.price
-      } else if (sortByPrice === 'DESC') {
-        return b.price - a.price
-      }
-      return 0
-    })
-    .filter((product) => selectCategory[product.category]) */
-
-  /*   const handleSortByChange = (e) => {
-    setSortByPrice(e.target.value)
-  } */
+  const categorieSkeletons = new Array(4).fill(null)
 
   return (
-    <div className='grid grid-cols-2 my-8'>
-      {data?.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className='grid py-4 grid-cols-2 w-full place-items-center min-h-[calc(100vh-112px)]'>
+      {isPending
+        ? categorieSkeletons.map((_el, index) => {
+            return (
+              <div key={index} className='h-80 w-full max-w-[180px] rounded-lg'>
+                <Skeleton className='h-full w-full' />
+              </div>
+            )
+          })
+        : data?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
     </div>
   )
 }

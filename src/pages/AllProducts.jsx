57d368e149d21015c1ecb776cc6cae52bee97a50
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-06-25 08:47:31 -0300

    mb8

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -17,7 +17,7 @@ const AllProducts = () => {
       {isPending
         ? categorieSkeletons.map((_el, index) => {
             return (
-              <div key={index} className='h-80 mb-4 w-full max-w-[180px] rounded-xl'>
+              <div key={index} className='h-80 mb-8 w-full max-w-[180px] rounded-xl'>
                 <Skeleton className='h-full w-full' />
               </div>  
             )
